3e0b0211c938bf6b48d023a07ebb366e0881759e
ghost-sys-coder <juniorbeast177@gmail.com>
2024-02-15 19:32:51 +0300

    Fix Fetching Categories

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,9 +1,10 @@
 import Category from "../models/Category.js";
-
+import { mongoConnect } from "../database/mongoose.js";
 /**
  * ! Fetch Categories
  */
 const FetchCategories = async (req, res) => {
+    await mongoConnect();
     try {
         const categories = await Category.find({}).populate("parentCategory");
         res.status(200).json(categories);
